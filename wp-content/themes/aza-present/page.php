73d8bc071f3a2292e86b28fbba44e11ff76473fa
Alexander Snarsky <snarsky93@gmail.com>
2021-08-07 17:00:22 +0300

    feat: add template for inner pages

---
+++ wp-content/themes/aza-present/page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div class="ip-content">
+  <div class="ip-content__head">
+    <div class="container">
+      <h1 class="ip-content__title"><?php the_title() ?></h1>
+    <?php
+    if (function_exists('yoast_breadcrumb')) {
+      yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+    }
+    ?>
+    </div>
+  </div>
+</div>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+<?php endwhile;
+wp_reset_query();
+?>
+
+<?php get_footer(); ?>
