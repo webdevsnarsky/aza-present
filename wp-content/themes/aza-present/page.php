ccfe3b509e108135b207254416ac985481df6dc2
Alexander Snarsky <snarsky93@gmail.com>
2021-08-07 21:58:17 +0300

    feat: add posts template

--- wp-content/themes/aza-present/page.php
+++ wp-content/themes/aza-present/page.php
@@ -3,12 +3,12 @@
 <div class="ip-content">
   <div class="ip-content__head">
     <div class="container">
-      <h1 class="ip-content__title"><?php the_title() ?></h1>
     <?php
     if (function_exists('yoast_breadcrumb')) {
       yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
     }
     ?>
+      <h1 class="ip-content__title"><?php the_title() ?></h1>
     </div>
   </div>
 </div>
--- wp-content/themes/aza-present/single-post.php
+++ wp-content/themes/aza-present/single-post.php
@@ -1,4 +1,25 @@
 <?php get_header(); ?>
 
+<div class="ip-content">
+  <div class="container">
+    <div class="ip-content__head">
+      <?php
+      if (function_exists('yoast_breadcrumb')) {
+        yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+      }
+      ?>
+      <h1 class="ip-content__title"><?php the_title() ?></h1>
+    </div>
+
+
+    <div class="ip-content__body">
+      <?php while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+      <?php endwhile;
+      wp_reset_query();
+      ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
