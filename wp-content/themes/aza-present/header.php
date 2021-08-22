bc19641727434f74c904a3d58f7c4f5c79902829
Alexander Snarsky <snarsky93@gmail.com>
2021-08-22 18:01:48 +0300

    refactor: change lang of woocommerce

--- wp-content/themes/aza-present/header.php
+++ wp-content/themes/aza-present/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="<?php echo bloginfo('template_url'); ?>/assets/img/favicons/favicon.ico" type="image/x-icon">
-  <title>Магазин подарков</title>
+  <title><?php bloginfo('name'); echo " | "; bloginfo('description'); ?></title>
   <?php wp_head(); ?>
 
 </head>
